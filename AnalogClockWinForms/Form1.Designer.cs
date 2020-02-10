2cfa6c239773020d298d6b4ba536eeb63ceba614
Louger910 <lougercraft@gmail.com>
2020-02-10 20:48:36 +0200

    Not done

--- AnalogClockWinForms/Form1.Designer.cs
+++ AnalogClockWinForms/Form1.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.StartPauseButton = new System.Windows.Forms.Button();
+            this.ResetButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // StartPauseButton
+            // 
+            this.StartPauseButton.Location = new System.Drawing.Point(13, 189);
+            this.StartPauseButton.Name = "StartPauseButton";
+            this.StartPauseButton.Size = new System.Drawing.Size(75, 23);
+            this.StartPauseButton.TabIndex = 0;
+            this.StartPauseButton.Text = "Start/Pause";
+            this.StartPauseButton.UseVisualStyleBackColor = true;
+            this.StartPauseButton.Click += new System.EventHandler(this.StartPauseButton_Click);
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(13, 219);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(75, 23);
+            this.ResetButton.TabIndex = 1;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.StartPauseButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
@@ -43,6 +67,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button StartPauseButton;
+        private System.Windows.Forms.Button ResetButton;
     }
 }
 
--- AnalogClockWinForms/Form1.cs
+++ AnalogClockWinForms/Form1.cs
@@ -39,13 +39,21 @@ namespace AnalogClockWinForms
             g.FillPolygon(solidBrush, points);
         }
 
-        private void InitializeTransform(Graphics g)
+        private void InitializeTransform(Graphics g, int a, int b)
         {
             g.ResetTransform();
-            g.TranslateTransform(ClientSize.Width / 2, ClientSize.Height / 2);
+            g.TranslateTransform(ClientSize.Width / a, ClientSize.Height / b);
             float scale = System.Math.Min(ClientSize.Width, ClientSize.Height) / 200.0f;
             g.ScaleTransform(scale, scale);
         }
+
+        //private void InitializeTransformStopWatch(Graphics g)
+        //{
+        //    g.ResetTransform();
+        //    g.TranslateTransform(ClientSize.Width / 4, ClientSize.Height / 4);
+        //    float scale = System.Math.Min(ClientSize.Width, ClientSize.Height) / 200.0f;
+        //    g.ScaleTransform(scale, scale);
+        //}
 
         private void OnTimer(object sender, EventArgs e)
         {
@@ -61,7 +69,7 @@ namespace AnalogClockWinForms
             SolidBrush blue = new SolidBrush(Color.Blue);
             SolidBrush white = new SolidBrush(Color.White);
             SolidBrush black = new SolidBrush(Color.Black);
-            InitializeTransform(g);
+            InitializeTransform(g,2,2);
             //draw border
             for (int i = 0; i < 120; i++)
             {
@@ -85,23 +93,62 @@ namespace AnalogClockWinForms
             int secondInt = nowDateTime.Second;
             int minuteInt = nowDateTime.Minute;
             int hourInt = nowDateTime.Hour % 12;
-            InitializeTransform(g);
+            InitializeTransform(g, 2, 2);
             //hour hand draw
             g.RotateTransform((hourInt * 30) + (minuteInt / 2));
             DrawHand(g, blue, 70, false);
-            InitializeTransform(g);
+            InitializeTransform(g, 2, 2);
             //minute hand draw
             g.RotateTransform((minuteInt * 6f) + secondInt / 10f);
             DrawHand(g, red, 100, false);
-            InitializeTransform(g);
+            InitializeTransform(g,2,2);
             //second hand draw
             g.RotateTransform(secondInt * 6);
             DrawHand(g, green, 100, true);
+
+            //STOPWATCH
+
+            Graphics gs = e.Graphics;
+            gs.SmoothingMode = SmoothingMode.AntiAlias;
+            InitializeTransform(gs, 9, 5);
+
+            //draw border
+            for (int i = 0; i < 120; i++)
+            {
+                gs.RotateTransform(5.0f);
+                gs.FillRectangle(black, 30, -5, 3, 10);
+            }
+            //draw minute mark
+            for (int i = 0; i < 60; i++)
+            {
+                gs.RotateTransform(6.0f);
+                gs.FillRectangle(black, 25, -10, 5, 1);
+            }
+            int secondIntS = 0;
+            
+            secondIntS++;
+
+            InitializeTransform(gs, 9, 5);
+            //second hand draw
+            gs.RotateTransform(secondIntS * 6);
+            DrawHand(gs, green, 30, true);
+
+
             red.Dispose();
             green.Dispose();
             blue.Dispose();
             white.Dispose();
             black.Dispose();
+        }
+
+        private void StartPauseButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
